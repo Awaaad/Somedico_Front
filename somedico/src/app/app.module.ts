4558ecf5e360a156332621816abb0fbf5e882ee9
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2020-03-15 22:04:25 +0400

    feat: structuring project in folders, pages and components

--- somedico/src/app/app.module.ts
+++ somedico/src/app/app.module.ts
@@ -9,8 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HeaderNavComponent } from '../app/shared/components/header-nav/header-nav.component';
+import { SideNavComponent } from '../app/shared/components/side-nav/side-nav.component';
+import { FooterComponent } from '../app/shared/components/footer/footer.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderNavComponent,
+    SideNavComponent,
+    FooterComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
