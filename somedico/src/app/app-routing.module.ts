ffbfd0bb4e1100e475aad3e53fb4d06ede8fc731
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2020-03-16 10:32:01 +0400

    feat: added side nav

--- somedico/src/app/app-routing.module.ts
+++ somedico/src/app/app-routing.module.ts
@@ -3,10 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./pages/products/products.module').then( m => m.ProductsPageModule)
   },
 ];
 
